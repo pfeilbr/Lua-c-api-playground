712c5888887b0bd9fb9c868ff70f7ab4c46776b6
Brian Pfeil <brian.pfeil@gmail.com>
2017-12-31 11:25:56 -0500

    Initial Commit

---
+++ Lua-c-api-playground/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Lua-c-api-playground
+//
+//  Created by Brian Pfeil on 12/31/17.
+//  Copyright © 2017 Brian Pfeil. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
