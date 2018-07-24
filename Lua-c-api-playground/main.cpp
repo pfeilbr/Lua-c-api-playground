62d89dc96c9c04c952831ece8817b93ce3bafe32
Brian <brian.pfeil@gmail.com>
2018-07-24 10:45:34 -0400

    init

--- Lua-c-api-playground/main.cpp
+++ Lua-c-api-playground/main.cpp
@@ -8,8 +8,44 @@
 
 #include <iostream>
 
+#include <stdio.h>
+#include <string.h>
+
+#include <lua.hpp>
+
+// call the print function with a string argument
+static void printExample(lua_State *L) {
+    lua_getglobal(L, "print");
+    lua_pushstring(L, "enter your lua code and press enter");
+    lua_pcallk(L, 1, 0, 0, 0, 0);
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    char buff[256];
+    int error;
+
+    lua_State *L = luaL_newstate();
+    
+    luaopen_base(L);             /* opens the basic library */
+    luaopen_table(L);            /* opens the table library */
+    luaopen_io(L);               /* opens the I/O library */
+    luaopen_string(L);           /* opens the string lib. */
+    luaopen_math(L);             /* opens the math lib. */
+    
+    printExample(L);
+    
+    while (fgets(buff, sizeof(buff), stdin) != NULL) {
+        error = luaL_loadbuffer(L, buff, strlen(buff), "line") ||
+        lua_pcall(L, 0, 0, 0);
+        
+        if (error) {
+            fprintf(stderr, "%s", lua_tostring(L, -1));
+            lua_pop(L, 1);  /* pop error message from the stack */
+        }
+    }
+    
+    lua_close(L);
+    
     return 0;
 }
---
+++ README.md
@@ -0,0 +1,13 @@
+# Lua C API Playground
+
+learn and experiment with [Lua C API](https://www.lua.org/manual/5.3/#api)
+
+see [Lua-c-api-playground/main.cpp](Lua-c-api-playground/main.cpp)
+
+## Xcode Project Config
+
+![](https://www.evernote.com/l/AAFJER84WslG6L2PE5NmRurmTYUFH3LWyEkB/image.png)
+
+![](https://www.evernote.com/l/AAFsRcTOjdtLgJy3ALariLx7RGl_TKAfYzAB/image.png)
+
+![](https://www.evernote.com/l/AAHVh3E76RlO1JPdiaVZXEAtsiNhGrU20PEB/image.png)
